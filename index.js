7728f5b5f7c0d233f7b387a4832b1dc8f521fe8b
yuandong <yuandong618@gmail.com>
2015-05-17 12:38:53 +0800

    Finish Lesson5 Implement the class inheritance

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var Class=function (properties){
-	//constructor
+var Class=function (properties,parent){
+
+	//constructor ,also means current class
 	function ctor(){
 		if(properties.hasOwnProperty("initialize")){
-			properties["initialize"].apply(this,arguments);
+			properties["initialize"].apply(this,arguments);//
 		}
-	}
 
+	}
+	//parent exist 
+	if(parent){
+		ctor.prototype=Object.create(parent.prototype);//points to parent
+		ctor.prototype.constructor=ctor;
+	}
+	ctor.__super__=parent||Object;
 	//traverse function
 	for(var key in properties){
 		if(!properties.hasOwnProperty)
@@ -20,6 +27,20 @@ var Class=function (properties){
 
 		ctor.prototype[key]=properties[key];
 	}
+	//super method
+	ctor.prototype.super=function (){
+		var currentClass=ctor;
+
+		return function(methodName){
+			var oldClass=currentClass;
+			currentClass=oldClass.__super__;
+			var result= currentClass.prototype[methodName].apply(this,[].slice.call(arguments, 1));
+			//recover
+			currentClass=oldClass;
+			return result;
+		}
+	}();
+
 	return ctor;
 }
 
